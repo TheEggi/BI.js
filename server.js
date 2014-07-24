0e03e68be3d545a0d2180cbbd699c66245b4efce
TheEggi <thomas.eckerstorfer@gmail.com>
2014-07-24 22:04:07 +0200

    fixed package.json and added startup script

---
+++ server.js
@@ -0,0 +1,17 @@
+var connect = require('connect'),
+    port = Number(process.env.PORT || 5000);
+
+
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+});
+
+server.listen(port);
